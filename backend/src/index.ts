a5a2c7c275d2289e2a608fba1324ef94705ce861
Tyler Chen <chentyler52@gmail.com>
2023-01-23 00:38:31 -0700

    Added DB Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 
 const app: Express = express();
 const port = 3000;
+
+try {
+	await mongoose.connect("mongodb://127.0.0.1:27017/yyc-rents");
+	console.log("Database Connected!");
+} catch (error) {
+	console.log("Error Connecting!");
+}
+
 
 import userRoutes from "../routes/users.js";
 
