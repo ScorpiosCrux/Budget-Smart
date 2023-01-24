77d43e8e1c3ba57824d535ce1b56b2eb6d9f8b31
Tyler Chen <chentyler52@gmail.com>
2023-01-24 00:54:18 -0700

    Added Printouts for API

--- backend/controllers/users.ts
+++ backend/controllers/users.ts
@@ -1,24 +1,25 @@
 import { Request, Response } from "express";
 import User from "../models/users.js";
 
-
 export const registerNewUser = async (req: Request, res: Response) => {
+	console.log(req.body);
 	try {
 		const { email, password } = req.body;
 		const username = email;
-		const newUser = new User({email, username, password})
+		const newUser = new User({ email, username, password });
 		const registeredUser = await User.register(newUser, password);
 		res.send("User Registerd");
 	} catch (error) {
-		console.log(error)
+		console.log(error);
 	}
 	console.log("RegisterNewUser");
 };
 
 export const loginUserForm = (req: Request, res: Response) => {
-	res.send("Login GET Route!")
-}
+	res.send("Login GET Route!");
+};
 
 export const loginUser = (req: Request, res: Response) => {
-	res.send("Logged In!")
-}
+	console.log(req.body);
+	res.send("Logged In!");
+};
