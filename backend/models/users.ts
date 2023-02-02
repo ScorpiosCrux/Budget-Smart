dd19985af8ed9a68d331ecfa122050215195642f
Tyler Chen <chentyler52@gmail.com>
2023-02-01 20:14:50 -0700

    Added refreshTokens and a toJSON function

--- backend/models/users.ts
+++ backend/models/users.ts
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 import passport from "passport-local-mongoose";
 
+const SessionSchema = new mongoose.Schema({
+	refreshToken: {
+	  type: String,
+	  default: "",
+	},
+  })
+
 const UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -9,8 +16,23 @@ const UserSchema = new mongoose.Schema({
 	},
 	displayName: {
 		type: String,
+		default: "",
+	},
+	authStrategy: {
+		type: String,
+		default: "local"
+	},
+	refreshToken: {
+		type: [SessionSchema], // supports multiple sign ins from multiple devices at the same time
 	}
 });
+
+UserSchema.set("toJSON", {
+	transform: function (doc, ret, options) {
+		delete ret.refreshToken
+		return ret
+	}
+})
 
 // Username and Password is added here.
 UserSchema.plugin(passport);
