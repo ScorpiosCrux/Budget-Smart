7a8f39cb6a954feb032f8483cdffa7dc905b746c
Tyler Chen <chentyler52@gmail.com>
2023-02-16 15:18:57 -0700

    Added typography constants

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,5 @@
 // Theme Context https://reactjs.org/docs/context.html Good example
 
-
 export const themes = {
 	light: {
 		accent: "#2AB514",
@@ -8,6 +7,21 @@ export const themes = {
 		primary: "#FFF",
 		secondary: "#939393",
 	},
+};
 
-
+/* Add Constants for Text sizes */
+export const typography = {
+	title: {
+		fontSize: "1.25rem",
+		fontWeight: "700"
+	},
+	regular: {
+		fontSize: "1rem",
+		fontWeight: "300"
+	},
+	bold: {
+		fontSize: "1rem",
+		fontWeight: "600"
+	}
 }
+
