73d3471d1a8753f9c935e56fe12093328b56228a
Tyler Chen <chentyler52@gmail.com>
2023-02-20 16:35:31 -0700

    controller: parse and save csv file

--- backend/src/controllers/transactions.ts
+++ backend/src/controllers/transactions.ts
@@ -1,6 +1,7 @@
 import Transaction from "../models/transactions.js";
 
 const TestTransaction = new Transaction({
+	userId: "1234121421",
 	date: "Feb 20, 2023",
 	description: "APPLE.COM/STORE",
 	price: "$32.29",
--- backend/src/models/transactions.ts
+++ backend/src/models/transactions.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const TransactionSchema = new mongoose.Schema({
+	userId: {
+		type: String,
+		required: true,
+	},
 	date: {
 		type: String,
 		required: true,
