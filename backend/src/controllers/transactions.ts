f5fc702be262bec6b45d3d66e0e5aec1440468e7
Tyler Chen <chentyler52@gmail.com>
2023-02-20 22:18:17 -0700

    Got Transactions based on userId

--- backend/src/controllers/transactions.ts
+++ backend/src/controllers/transactions.ts
@@ -1,13 +1,40 @@
+import * as csv from "@fast-csv/parse";
 import Transaction from "../models/transactions.js";
 
-const TestTransaction = new Transaction({
-	userId: "1234121421",
-	date: "Feb 20, 2023",
-	description: "APPLE.COM/STORE",
-	price: "$32.29",
-});
+export const importTransactions = async () => {
+	// Get File and Save
 
-TestTransaction.save((err: any) => {
-	if (err) console.log(err);
-	else console.log("Successfully Saved Transaction")
-});
+	// Read File (after checking for possible breaking characters or code)
+
+	// Name this file transactions/userId.csv
+	const csv_file_path = "/Users/vivid/Code/Smart-Budget/backend/src/utilities/transactions.csv";
+
+	csv.parseFile(csv_file_path)
+		.on("error", (error) => console.error(error))
+		.on("data", (row) => {
+			// Specific for TD Canada Trust
+			const transaction = new Transaction({
+				userId: "1234",
+				date: row[0],
+				description: row[1],
+				category: "",
+				price: row[2],
+			});
+
+			transaction.save((err: any) => {
+				if (err) console.log(err);
+				else {
+					console.log("Successfully Saved Transaction");
+					console.log(row);
+				}
+			});
+		})
+		.on("end", (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
+};
+
+export const getTransactions = async () => {
+	const userId = "1234";
+
+	const posts = await Transaction.find({ userId: userId });
+	console.log("Done")
+};
