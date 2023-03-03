536ace08f8761e5c5bff63a9f9fcb6b5e71099e7
Tyler Chen <chentyler52@gmail.com>
2023-03-02 17:41:27 -0700

    possible authcontext?

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -1,12 +1,26 @@
+import { useAuth } from "hooks/useAuth";
 import { User } from "hooks/useUser";
 import { createContext } from "react";
+import { UserContext } from "./UserContext";
 
 interface AuthContext {
 	user: User | null;
-	setUser: (user: User | null) => void;
+	login: (user: User) => void;
+	logout: () => void;
 }
 
 export const AuthContext = createContext<AuthContext>({
 	user: null,
-	setUser: () => {},
+	login: () => {},
+	logout: () => {},
 });
+
+type Props = {
+	children: JSX.Element;
+};
+
+export const AuthContextProvider = (props: Props) => {
+	const { user, login, logout } = useAuth();
+
+	return <AuthContext.Provider value={{ user, login, logout }}>{props.children}</AuthContext.Provider>;
+};
