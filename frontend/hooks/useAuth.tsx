c6cbf3c5c34e9b987999a18df3c661bd320953fd
Tyler Chen <chentyler52@gmail.com>
2023-03-02 20:06:16 -0700

    useAuth login method created

--- frontend/hooks/useAuth.tsx
+++ frontend/hooks/useAuth.tsx
@@ -1,4 +1,5 @@
 import { AuthUser } from "@/types";
+import axios from "axios";
 import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 import { useUser } from "./useUser";
@@ -14,8 +15,32 @@ export const useAuth = () => {
 		}
 	}, []);
 
-	const login = (user: AuthUser) => {
-		addUser(user);
+	const login = (email: string, password: string) => {
+		axios({
+			method: "post",
+			data: {
+				username: email,
+				password: password,
+			},
+			withCredentials: true,
+			url: process.env.NEXT_PUBLIC_API_ENDPOINT + "/auth/login",
+		})
+			.then((res) => {
+				console.log("Data: ");
+				console.log(res.data);
+				const authUser : AuthUser = {
+					...res.data,
+					isLoggedIn: true,
+					displayName: "Display Name",
+					username: "Username"
+				}
+				addUser(authUser)
+				console.log(user)
+			})
+			.catch((error) => {
+				console.log("error")
+			});
+		// addUser(user);
 	};
 
 	const logout = () => {
