cd8bd931f02027e0ca4bb93d4aa1c00ee3053425
Tyler Chen <website@tylerchen.ca>
2023-04-27 21:20:25 -0600

    clean up

--- frontend/src/components/widgets/CategoriesWidget/Category.tsx
+++ frontend/src/components/widgets/CategoriesWidget/Category.tsx
@@ -3,72 +3,74 @@ import { useDrop } from "react-dnd";
 import { ICategory } from "@/types";
 import StyledLine from "@/components/core/StyledLine";
 import {
-	StyledCategoryHeader,
-	StyledCategory,
-	StyledCategoryContent,
-	StyledCategoryInfo,
-	StyledCategoryDNDContainer,
+  StyledCategoryHeader,
+  StyledCategory,
+  StyledCategoryContent,
+  StyledCategoryInfo,
+  StyledCategoryDNDContainer,
 } from "./CategoryStyledComponents";
-
-interface Props {
-	index: number;
-	category: ICategory;
-}
 
 /* MOVE THIS TO ANOTHER FILE */
 const ItemTypes = {
-	TRANSACTION: "transaction",
+  TRANSACTION: "transaction",
 };
 
+interface Props {
+  category: ICategory;
+}
 const CategoryComponent = (props: Props) => {
-	const [{ canDrop, isOver }, drop] = useDrop(() => ({
-		accept: ItemTypes.TRANSACTION,
-		drop: () => ({
-			category: props.category,
-		}),
-		collect: (monitor) => ({
-			isOver: monitor.isOver(),
-			canDrop: monitor.canDrop(),
-		}),
-	}));
+  const { category } = props;
+  const { _id, userId, name, budget, remainingBudget, totalSpent, remainingBudgetPerDay } =
+    category;
 
-	const isActive = canDrop && isOver;
-	let backgroundColor = themes.light.primaryBackground;
-	if (isActive) {
-		backgroundColor = themes.light.accent;
-	} else if (canDrop) {
-		backgroundColor = themes.light.secondaryBackground;
-	}
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: ItemTypes.TRANSACTION,
+    drop: () => ({
+      category: props.category,
+    }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }));
 
-	return (
-		<StyledCategory index={props.index}>
-			<StyledCategoryHeader>
-				<div className="info">
-					<span className="title">{props.category.name}</span>
-					{/* <img src="check.svg" alt="checkmark" /> */}
-				</div>
-			</StyledCategoryHeader>
-			<StyledLine />
-			<StyledCategoryContent ref={drop} backgroundColor={backgroundColor}>
-				{canDrop ? (
-					<StyledCategoryDNDContainer>
-						<div>Drop</div>
-						<div>Transaction</div>
-						<div>Here</div>
-					</StyledCategoryDNDContainer>
-				) : (
-					<StyledCategoryInfo>
-						<span>Budget </span>
-						<span>${props.category.budget.toFixed(2)}</span>
-						<span>Total</span>
-						<span>${props.category.totalSpent.toFixed(2)}</span>
-						<span>Remaining</span>
-						<span>${props.category.remainingBudget.toFixed(2)}</span>
-					</StyledCategoryInfo>
-				)}
-			</StyledCategoryContent>
-		</StyledCategory>
-	);
+  const isActive = canDrop && isOver;
+  let backgroundColor = themes.light.primaryBackground;
+  if (isActive) {
+    backgroundColor = themes.light.accent;
+  } else if (canDrop) {
+    backgroundColor = themes.light.secondaryBackground;
+  }
+
+  return (
+    <StyledCategory>
+      <StyledCategoryHeader>
+        <div className="info">
+          <span className="title">{name}</span>
+          {/* <img src="check.svg" alt="checkmark" /> */}
+        </div>
+      </StyledCategoryHeader>
+      <StyledLine />
+      <StyledCategoryContent ref={drop} backgroundColor={backgroundColor}>
+        {canDrop ? (
+          <StyledCategoryDNDContainer>
+            <div>Drop</div>
+            <div>Transaction</div>
+            <div>Here</div>
+          </StyledCategoryDNDContainer>
+        ) : (
+          <StyledCategoryInfo>
+            <span>Budget </span>
+            <span>${budget.toFixed(2)}</span>
+            <span>Total</span>
+            <span>${totalSpent.toFixed(2)}</span>
+            <span>Remaining</span>
+            <span>${remainingBudget.toFixed(2)}</span>
+          </StyledCategoryInfo>
+        )}
+      </StyledCategoryContent>
+    </StyledCategory>
+  );
 };
 
 export default CategoryComponent;
--- frontend/src/components/widgets/CategoriesWidget/CategoryStyledComponents.tsx
+++ frontend/src/components/widgets/CategoriesWidget/CategoryStyledComponents.tsx
@@ -15,7 +15,7 @@ export const StyledCategories = styled.div`
 
 /* ========= Category Components =========*/
 
-export const StyledCategory = styled.div<{ index: number }>`
+export const StyledCategory = styled.div`
 	width: 100%;
 	height: 10rem;
 
