0b6a85b67fa71ae5aeb799270a071ad9908b0140
Tyler Chen <website@tylerchen.ca>
2023-05-03 18:53:43 -0600

    created styled mui components

--- frontend/src/components/core/DatePicker.tsx
+++ frontend/src/components/core/DatePicker.tsx
@@ -1,11 +1,12 @@
 import { DatePicker } from "@mui/x-date-pickers";
 import { useField, useFormikContext } from "formik";
+import { StyledDatePicker } from "./StyledMUI";
 
 export const DatePickerField = (props: any) => {
 	const { setFieldValue } = useFormikContext();
 	const [field] = useField(props);
 	return (
-		<DatePicker
+		<StyledDatePicker
 			{...field}
 			{...props}
 			selected={(field.value && new Date(field.value)) || null}
---
+++ frontend/src/components/core/StyledMUI.tsx
@@ -0,0 +1,7 @@
+import { DatePicker } from "@mui/x-date-pickers";
+import styled from "styled-components";
+
+
+export const StyledDatePicker = styled(DatePicker)({
+	marginBottom: "20px"
+})
